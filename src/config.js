54969da372bf860b34ed57554c65fbe6208ede62
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-08-18 06:58:19 -0400

    added note and end point congif

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || '82ec702e-81ae-4bd3-bb15-a26cff58bd43',
+  API_ENDPOINT:process.env.API_ENDPOINT || 'http://localhost:8000/'
 }
