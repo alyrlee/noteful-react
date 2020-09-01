5610ab78aabc6e08531cab58ca27535e4768126d
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-09-01 03:14:36 -0400

    updated folders, notes

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || '82ec702e-81ae-4bd3-bb15-a26cff58bd43',
-  // API_ENDPOINT:process.env.API_ENDPOINT || 'https://fierce-taiga-41664.herokuapp.com/api',
-  API_ENDPOINT:process.env.API_ENDPOINT || 'http://localhost:8000/api'
+  API_ENDPOINT:process.env.API_ENDPOINT || 'https://fierce-taiga-41664.herokuapp.com/api',
+  // API_ENDPOINT:process.env.API_ENDPOINT || 'http://localhost:8000/api'
 }
