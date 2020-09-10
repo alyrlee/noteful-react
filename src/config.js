4f7ba1ff578c8ca968b1291207a8cd069cdcccef
Ashley Lee <53842786+alyrlee@users.noreply.github.com>
2020-09-09 20:35:18 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || '82ec702e-81ae-4bd3-bb15-a26cff58bd43',
+  API_TOKEN: process.env.API_TOKEN || 'your token',
   API_ENDPOINT:process.env.API_ENDPOINT || 'https://fierce-taiga-41664.herokuapp.com/api'
 }
